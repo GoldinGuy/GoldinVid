cb756a05f1fc23ddb84e66436cc6cbe8499dce6e
Seth <sethgoldin27@gmail.com>
2021-04-03 22:11:58 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import VideoChat from "catalyst-vc-react";
+import "./App.css";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const location = useLocation();
+	return (
+		<VideoChat
+			sessionKey={location.pathname.substring(1)}
+			uniqueAppId="fe1c7514-1b51-417c-a111-cf0d0deaa112"
+			themeColor="#1E40AF"
+			// defaults={{ audioOn: false }}
+			onEndCall={() => (window.location.href = "https://seth.goldin.io/")}
+		/>
+	);
 }
 
 export default App;
