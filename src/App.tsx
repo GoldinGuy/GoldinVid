f7bd0a98a3dbcf2fbd450ae5a55c9985e438d6b3
Seth <sethgoldin27@gmail.com>
2021-04-04 01:55:07 -0400

    update to use url props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import VideoChat from "catalyst-vc-react";
+import React from "react";
 import "./App.css";
 
 function App() {
+	const [showVideoCall, setShowVideoCall] = React.useState(true);
 	return (
-		<VideoChat
-			sessionKey={window.location.href.substring(
-				window.location.href.lastIndexOf("/") + 1
+		<>
+			<button
+				style={{ position: "absolute", top: 0, left: 0, zIndex: 10000 }}
+				onClick={() => setShowVideoCall(!showVideoCall)}
+			>
+				toggle on
+			</button>
+			{showVideoCall ? (
+				<VideoChat
+					sessionKey={window.location.href.substring(
+						window.location.href.lastIndexOf("/") + 1
+					)}
+					uniqueAppId="63431a59-3548-4aa2-9bdb-5d74a49c5d9d"
+					themeColor="#3a63e8"
+					// defaults={{ audioOn: false }}
+					onEndCall={() => (window.location.href = "https://seth.goldin.io/")}
+				/>
+			) : (
+				""
 			)}
-			uniqueAppId="63431a59-3548-4aa2-9bdb-5d74a49c5d9d"
-			themeColor="#3a63e8"
-			// defaults={{ audioOn: false }}
-			onEndCall={() => (window.location.href = "https://seth.goldin.io/")}
-		/>
+		</>
 	);
 }
 
